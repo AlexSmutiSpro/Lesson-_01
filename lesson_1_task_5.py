7e3b99af878e85f035a4e41057180bc2909e1992
AlexSmutiSpro <smuti6960@gmail.com>
2026-01-27 06:15:58 +0700

    Add files via upload

---
+++ lesson_1_task_5.py
@@ -0,0 +1,75 @@
+def num():
+    print(8, end='')
+
+
+num()
+
+
+def num():
+    print(8, end='')
+
+
+num()
+
+
+def num():
+    print(0, end='')
+
+
+num()
+
+
+def num():
+    print(0, end='')
+
+
+num()
+
+
+def num():
+    print(5, end='')
+
+
+num()
+
+
+def num():
+    print(5, end='')
+
+
+num()
+
+
+def num():
+    print(5, end='')
+
+
+num()
+
+
+def num():
+    print(3, end='')
+
+
+num()
+
+
+def num():
+    print(5, end='')
+
+
+num()
+
+
+def num():
+    print(3, end='')
+
+
+num()
+
+
+def num():
+    print(5)
+
+
+num()
